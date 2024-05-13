a034df5715849b472e9d116d11d283abfb182f6e
LeventeCzegledi-Hiya <levente.czegledi@hiya.com>
2024-05-13 17:05:42 +0200

    add push notifications

--- lib/pages/profile/profile_provider.dart
+++ lib/pages/profile/profile_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:codeblurb_mobile/network/firebase_api.dart';
 import 'package:codeblurb_mobile/providers.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -13,10 +14,16 @@ class ProfileNotifier extends _$ProfileNotifier {
   }
 
   Future<void> logout() async {
+    await FirebaseApi.deleteTokenFromFirestore(
+      ref.read(isLoggedInProvider.notifier).getUsername(),
+    );
     ref.read(isLoggedInProvider.notifier).setLoggedIn(value: false);
   }
 
   Future<void> forceLogout() async {
+    await FirebaseApi.deleteAllTokensFromFirestore(
+      ref.read(isLoggedInProvider.notifier).getUsername(),
+    );
     ref
         .read(isLoggedInProvider.notifier)
         .setLoggedIn(value: false, force: true);
--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -3,10 +3,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:codeblurb_mobile/generated/assets.gen.dart';
 import 'package:codeblurb_mobile/hooks/use_colors.dart';
+import 'package:codeblurb_mobile/network/firebase_api.dart';
 import 'package:codeblurb_mobile/pages/shopping_cart/shopping_cart_provider.dart';
+import 'package:codeblurb_mobile/providers.dart';
 import 'package:codeblurb_mobile/routes/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @RoutePage()
@@ -18,6 +21,17 @@ class TabsPage extends HookConsumerWidget {
     final shoppingCartQuery = ref.watch(shoppingCartQueryProvider);
     final colors = useColors();
     final bottomNavbarColors = Theme.of(context).bottomNavigationBarTheme;
+
+    useEffect(
+      () {
+        FirebaseApi().initNotifications(
+          ref.read(isLoggedInProvider.notifier).getUsername(),
+        );
+        return null;
+      },
+      [],
+    );
+
     return AutoTabsScaffold(
       routes: const [
         HomeRoute(),
